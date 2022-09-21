88070ebb745b7b44608a9229c9f4f071bb1b0eed
HEEJINYU <hjyu@mfmkorea.com>
2022-09-21 15:51:44 +0900

    장부 생성, 업데이트 및 삭제

---
+++ src/BookStore.Blazor/BookStoreBlazorAutoMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using BookStore.Books;
+using AutoMapper;
+
+namespace BookStore.Blazor
+{
+    public class BookStoreBlazorAutoMapperProfile : Profile
+    {
+        public BookStoreBlazorAutoMapperProfile()
+        {
+            CreateMap<BookDto, CreateUpdateBookDto>();
+            //automapper.profile을 상속함으로써 사용가능
+        }
+
+
+    }
+}
